ff1bb437a0a8370faa8bd7719b374af876667ab7
Taylor <93610040+TO-Point@users.noreply.github.com>
2023-11-15 13:30:00 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+// Function to set a cookie
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+// Function to get a cookie by name
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+// Function to check if the acknowledged cookie exists
+function checkCookie() {
+  var acknowledged = getCookie("acknowledged");
+  if (acknowledged) {
+    // If the acknowledged cookie exists, hide the cookie wrapper
+    document.getElementById('cookie-wrapper').style.display = 'none';
+  } else {
+    // If the acknowledged cookie does not exist, add the show class
+    document.getElementById('cookie-wrapper').classList.add('show');
+  }
+}
+
+// Event listener for the acknowledge button
+document.addEventListener('DOMContentLoaded', function() {
+  var acknowledgeBtn = document.getElementById('cookie-acknowledge');
+  acknowledgeBtn.addEventListener('click', function() {
+    setCookie('acknowledged', 'true', 7); // Set the cookie for 7 days
+    document.getElementById('cookie-wrapper').style.display = 'none';
+  });
+
+  checkCookie(); // Check the cookie when the DOM is loaded
+});
